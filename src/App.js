a7e924bdaffde783935fc5e1e0c9eaea79bfaa39
xw <23066125+xwang1000@users.noreply.github.com>
2019-03-31 14:40:30 -0700

    Setup Redux

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import { simpleAction } from './actions/simpleAction';
+
+const mapStateToProps = state => ({
+  ...state
+ })
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+ })
 
 class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+   }
+   
   render() {
     return (
       <div className="App">
@@ -21,9 +34,15 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <pre>
+        {
+          JSON.stringify(this.props)
+        }
+        </pre>
+        <button onClick={this.simpleAction}>Test redux action</button>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -6,18 +6,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // JavaScript entry point
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-
 ReactDOM.render(
  <Provider store={configureStore()}>
   <App />
  </Provider>,
  document.getElementById('root')
 );
+
+serviceWorker.unregister();
