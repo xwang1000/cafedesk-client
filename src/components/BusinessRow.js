8ef6234e7c5d1eb76b1bd19c543bd170dc885a97
xw <23066125+xwang1000@users.noreply.github.com>
2019-04-02 22:58:00 -0700

    Clean code

--- src/components/BusinessRow.js
+++ src/components/BusinessRow.js
@@ -3,18 +3,19 @@ import TagList from './TagList'
 import './Business.css'
 
 const BusinessRow = props => {
-
+  const { business } = props
+  
   return (
     <div className="business-row">
       <div className="business-row-col1">
-        <img className="business-row-image" src={props.business.image_url}></img>
+        <img className="business-row-image" src={business.image_url}></img>
       </div>
 
       <div className="business-row-col2">
-        <p className="business-row-name">{props.business.name}</p>
-        <p className="business-row-close">closes at {props.business.closing_time} pm</p>
-        <p className="business-row-distance">{props.business.distance} km away</p>
-        <TagList tags = {props.business.tags} />
+        <p className="business-row-name">{business.name}</p>
+        <p className="business-row-close">closes at {business.closing_time} pm</p>
+        <p className="business-row-distance">{business.distance} km away</p>
+        <TagList tags = {business.tags} />
       </div>
     </div>
   )
