dee1762419cc231e81eacd3fd3427c487821d497
xw <23066125+xwang1000@users.noreply.github.com>
2019-04-30 19:59:21 -0700

    Clean up code and change demo style

--- src/components/features/Demo.js
+++ src/components/features/Demo.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 import './Demo.css'
+import { toClassNames } from '../../utils'
 
 const Demo = () => {
   const [isOpen, toggleOpen] = useState(false)
 
-  const renderBody = () => {
-    if (isOpen) {
-      return <div className="demo__body">
+  const className = toClassNames({
+    'demo__body': true,
+    'demo__body--slide-in': isOpen
+  }) 
+
+  const getClass = isOpen ? 'open' : ''
+  
+  return (
+    <div className="demo">
+      <div className="demo__header" onClick={() => toggleOpen(!isOpen)}>
+        <i className={"fas fa-flag " + getClass}></i>
+      </div>
+
+      <div className={className}>
         <h1>CafeDesk</h1>
         <p>This app is for demo only. Although all data are real life information from Yelp and Google, we do not guarantee the data persistence of your information. </p>
         <p>Feel free to mess around with it. We sincerely hope that it helps you finding a good coffee shop to work at. Please submit any issues <a href="https://github.com/xwang1000/cafedesk-client/issues/new" target="_blank" rel="noopener noreferrer">here</a>.</p>
@@ -15,17 +27,6 @@ const Demo = () => {
           <a href="https://github.com/xwang1000/cafedesk-client" rel="noopener noreferrer" target="_blank"> here.</a>
         </p>
       </div>
-    }
-
-    return ''
-  }
-  return (
-    <div className="demo">
-      <div className="demo__header" onClick={() => toggleOpen(!isOpen)}>
-        <h1>DEMO</h1>
-      </div>
-
-      {renderBody()}
     </div>
   )
 }
