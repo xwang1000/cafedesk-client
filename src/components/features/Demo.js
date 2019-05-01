859d84d22e1ffe7cc8faad6ea2e5a6325590ea3b
xw <23066125+xwang1000@users.noreply.github.com>
2019-04-30 23:09:34 -0700

    Add art designer credits

--- src/components/features/Demo.js
+++ src/components/features/Demo.js
@@ -23,8 +23,10 @@ const Demo = () => {
         <p>This app is for demo only. Although all data are real life information from Yelp and Google, we do not guarantee the data persistence of your information. </p>
         <p>Feel free to mess around with it. We sincerely hope that it helps you finding a good coffee shop to work at. Please submit any issues <a href="https://github.com/xwang1000/cafedesk-client/issues/new" target="_blank" rel="noopener noreferrer">here</a>.</p>
         <p>
-          Check out our github repo 
-          <a href="https://github.com/xwang1000/cafedesk-client" rel="noopener noreferrer" target="_blank"> here.</a>
+          Check out our github repo <a href="https://github.com/xwang1000/cafedesk-client" rel="noopener noreferrer" target="_blank">here.</a>
+        </p>
+        <p>
+        Icons designed by <strong>Freepic</strong> and <strong>Icongeek26</strong> from <a href="https://www.flaticon.com/">Flaticon</a>
         </p>
       </div>
     </div>
